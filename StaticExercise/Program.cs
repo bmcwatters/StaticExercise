e0de2275646defdc4cbca7647715ea8cd5c7023b
bryan mcwatters <bmcwatters@comcast.net>
2021-12-10 11:48:11 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"Celcius {celsius}");
+            Console.WriteLine("------------");
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+
+
+
+
+
+        }
+    }
+}
